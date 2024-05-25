c553b311ec173e79e2e020a4ca1290ba767856f5
ZeynepDilanKilic <zeynepdilankilic@gmail.com>
2024-05-25 22:57:04 +0300

    Created WebApi project

---
+++ DataAccess/Concretes/BrandDal.cs
@@ -0,0 +1,32 @@
+﻿using DataAccess.Abstracts;
+using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concretes;
+
+public class BrandDal : IBrandDal
+{
+    List<Brand> brands;
+
+    public BrandDal()
+    {
+        brands = new List<Brand>();
+        brands.Add(new Brand { Id = 1, Name = "BMW", CreatedDate = DateTime.Now });
+        brands.Add(new Brand { Id = 2, Name = "Mercedes", CreatedDate = DateTime.Now });
+        brands.Add(new Brand { Id = 3, Name = "Alfa Romeo", CreatedDate = DateTime.Now });
+    }
+
+    public void Add(Brand brand)
+    {
+        brands.Add(brand);
+    }
+
+    public List<Brand> GetAll()
+    {
+        return brands;
+    }
+}
---
+++ WebApi/Controllers/BrandsController.cs
@@ -0,0 +1,37 @@
+﻿using Business.Dtos.Responses;
+using Business.Dtos.Requests;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Business.Abstracts;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : ControllerBase
+    {
+        private readonly IBrandService _brandService;
+
+
+        public BrandsController(IBrandService brandService)
+        {
+            _brandService = brandService;
+        }
+
+        [HttpPost]
+        public IActionResult Add(CreateBrandRequest createBrandRequest)
+        {
+            CreatedBrandResponse createBrandResponse = new CreatedBrandResponse();
+
+            return Ok(_brandService.Add(createBrandRequest));
+
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+            return Ok(_brandService.GetAll());
+        }
+    }
+}
